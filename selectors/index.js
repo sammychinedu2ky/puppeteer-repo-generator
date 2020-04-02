8071b78dbadab3e1c55e49cc8a6eee303c0a6cb5
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-04-02 22:54:33 +0100

    init

---
+++ selectors/index.js
@@ -0,0 +1,17 @@
+module.exports={
+    login_button: '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block',
+    username_field: '#login_field',
+    password_field: '#password',
+    otp: '#otp',
+    otp_error: '#js-flash-container > div > div',
+    otp_url: 'https://github.com/sessions/verified-device',
+    otp_button: '#login > div.auth-form-body.mt-3 > form > button',
+    new_repo: 'body > div.application-main > div > aside.team-left-column.col-12.col-md-4.col-lg-3.bg-white.border-right.border-bottom.hide-md.hide-sm > div.dashboard-sidebar.js-sticky.top-0.px-3.px-md-4.px-lg-4.overflow-auto > div.mb-3.Details.js-repos-container.mt-5 > div > h2 > a',
+    repo_name_field: '#repository_name',
+    repo_name_err: '#new_repository > div.owner-reponame.clearfix > auto-check > dl > dd.error > strong',
+    create_repo: '#new_repository > div.js-with-permission-fields > button',
+    repo_link: '#empty-setup-clone-url',
+    username: '',
+    password: '',
+    repo_name: ''
+}

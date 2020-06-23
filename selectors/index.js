d8a24810f0ffc1552f374150be6ad41e586bd8ce
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-06-23 07:56:09 +0100

    tfa url selector added

--- selectors/index.js
+++ selectors/index.js
@@ -4,6 +4,7 @@ module.exports={
     password_field: '#password',
     otp: '#otp',
     otp_error: '#js-flash-container > div > div',
+    sec_key_tfa_url: 'https://github.com/sessions/two-factor',
     otp_url: 'https://github.com/sessions/verified-device',
     otp_button: '#login > div.auth-form-body.mt-3 > form > button',
     new_repo: 'body > div.application-main > div > aside.team-left-column.col-12.col-md-4.col-lg-3.bg-white.border-right.border-bottom.hide-md.hide-sm > div.dashboard-sidebar.js-sticky.top-0.px-3.px-md-4.px-lg-4.overflow-auto > div.mb-3.Details.js-repos-container.mt-5 > div > h2 > a',
